40be328be5ee4afce5dccce0fb675369f8cd5d9c
KI-IAN <f.r.khan.trk@gmail.com>
2020-03-27 22:20:52 +0800

    ** Profile Pic page is completed. ** User can Tap on profile pic pop up and it takes user to profile pic page. ** Eventhandler function is added to navigate and pass data to profile pic page.

---
+++ AppReplica/AppReplica/ReplicatedUI/WhatsApp/ViewModels/ProfilePicViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppReplica.ReplicatedUI.WhatsApp.ViewModels
+{
+    public class ProfilePicViewModel : ViewModelBase
+    {
+
+        #region Fields
+
+        string _profilePicSource;
+
+        string _contact;
+
+        #endregion
+
+
+        #region Properties
+
+        public string ProfilePicSource
+        {
+
+            get
+            {
+                return _profilePicSource;
+            }
+
+            set
+            {
+                _profilePicSource = value;
+                base.OnPropertyChanged();
+            }
+        }
+
+
+        public string Contact
+        {
+
+            get
+            {
+                return _contact;
+            }
+
+            set
+            {
+                _contact = value;
+                base.OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+    }
+}
--- AppReplica/AppReplica/ReplicatedUI/WhatsApp/ViewModels/ProfilePopupViewModel.cs
+++ AppReplica/AppReplica/ReplicatedUI/WhatsApp/ViewModels/ProfilePopupViewModel.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Rg.Plugins.Popup.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using Xamarin.Forms;
 
 namespace AppReplica.ReplicatedUI.WhatsApp.ViewModels
 {
@@ -90,25 +92,51 @@ namespace AppReplica.ReplicatedUI.WhatsApp.ViewModels
         #endregion
 
 
+        #region Command Properties
+
+        public Command ShowProfilePicCommand { get; }
+
+        #endregion
+
+
         #region Constructor(s)
 
         //Empty Constructor
         public ProfilePopupViewModel()
         {
 
+            #region Attach Event Handlers
+
+            ShowProfilePicCommand = new Command((object parameter) => ShowProfilePic(parameter));
+
+            #endregion
+
         }
 
 
-        //public ProfilePopupViewModel(ProfilePopupViewModel data)
-        //{
-        //    this.ProfilePicSource = data.ProfilePicSource;
-        //    this.ContactName = data.ContactName;
-        //    this.ContactNumber = data.ContactNumber;
-        //    this.ProfilePicSource = data.ProfilePicSource;
-        //}
-
 
         #endregion
+
+
+        #region Event Handler Functions
+
+        private async void ShowProfilePic(object parameter)
+        {
+            var data = (ProfilePopupViewModel)parameter;
+
+            await PopupNavigation.Instance.PopAsync();      //removed the recently opened pop up
+
+            await Application.Current.MainPage.Navigation.PushAsync(new UIPages.ProfilePicPage(new ProfilePicViewModel()
+            {
+                Contact = data.Contact,
+                ProfilePicSource = data.ProfilePicSource,
+            }));
+
+
+        }
+
+        #endregion
+
 
     }
 }
